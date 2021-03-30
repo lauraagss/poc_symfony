5ec557012e33cd9cdbc2b09b80772e6d719b8c59
lauraagss <laura.goncalves@lyceestvincent.fr>
2021-03-30 19:59:03 +0200

    add project

--- src/DataFixtures/SerieFixtures.php
+++ src/DataFixtures/SerieFixtures.php
@@ -24,10 +24,10 @@ class SerieFixtures extends Fixture implements DependentFixtureInterface
                 ->setImage('https://www.google.com/search?q=netflix&rlz=1C1VDKB_frFR933FR933&sxsrf=ALeKk029kkvv5-G-Xr-aFceSmHUX3fIFNg:1617046108982&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjU1KSXntbvAhUj8-AKHRoRD3QQ_AUoA3oECAEQBQ&biw=1920&bih=937#imgrc=4H7caLjkjz8KzM')
                 ->setEpisodeCurrent(rand(1, 100))
                 ->setSeasonCurrent(rand(1, 10));
-            /** @var User $user */
+
             $user = $this->getReference('Utilisateur' . rand(0, 20));
 
-            /** @var Category $category */
+
             $category = $this->getReference('categ' . rand(1, 3));
 
             $serie
